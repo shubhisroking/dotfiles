09d8f39ebcccaa7d5188223cf3d1744d3acfd58f
Shubh <shubhisroking@proton.me>
2024-02-07 19:06:56 +0530

    add xmobar more properly
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -16,6 +16,7 @@ import XMonad.Util.Run
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
+import XMonad.Util.Loggers
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
@@ -260,11 +261,40 @@ myStartupHook = do
 
 -- Run xmonad with the settings you specify. No need to modify this.
 --
+myXmobarPP :: PP
+myXmobarPP = def
+    { ppSep             = magenta " • "
+    , ppTitleSanitize   = xmobarStrip
+    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+    , ppHidden          = white . wrap " " ""
+    , ppHiddenNoWindows = lowWhite . wrap " " ""
+    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    }
+  where
+    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+
+    -- | Windows should have *some* title, which should not not exceed a
+    -- sane length.
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+
+    blue, lowWhite, magenta, red, white, yellow :: String -> String
+    magenta  = xmobarColor "#ff79c6" ""
+    blue     = xmobarColor "#bd93f9" ""
+    white    = xmobarColor "#f8f8f2" ""
+    yellow   = xmobarColor "#f1fa8c" ""
+    red      = xmobarColor "#ff5555" ""
+    lowWhite = xmobarColor "#bbbbbb" ""
+
+
 main :: IO ()
 main = xmonad 
      . ewmhFullscreen 
      . ewmh
-     . xmobarProp 
+     . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey 
      $ defaults
 
 -- A structure containing your configuration settings, overriding
