97652be9832e27d6cf7eb04952c9e0d492d40cfe
Shubh <shubhisroking@proton.me>
2024-02-20 17:20:02 +0530

    [lvim] update conf
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- lvim/config.lua
+++ lvim/config.lua
@@ -2,10 +2,10 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-
+--
 lvim.plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "windwp/nvim-ts-autotag" },
   { "github/copilot.vim" }
 }
 
-lvim.colorscheme "catppuccin"
+lvim.builtin.treesitter.autotag.enable = true
