1dc282221ade2dd19c0e3c486e47df7c711d0129
Shubh <shubhisroking@proton.me>
2024-02-21 17:10:47 +0530

    [lvim] noicee
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -13,9 +13,26 @@ lvim.plugins = {
       end,
     },
   },
+  -- lazy.nvim
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      -- "rcarriga/nvim-notify",
+    }
+  },
   { "github/copilot.vim" }
 }
 
+vim.opt.confirm = true
 vim.g.copilot_assume_mapped = true
 lvim.format_on_save.enabled = true
 vim.opt.relativenumber = true -- relative line numbers
