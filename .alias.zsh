89dae8e5aefbbee95af1a9dda07da83a9c2d0345
Shubh <shubhisroking@proton.me>
2024-02-23 12:19:16 +0530

    goodbye lunarvim
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- .alias.zsh
+++ .alias.zsh
@@ -6,7 +6,7 @@ eval $(thefuck --alias)
 eval $(thefuck --alias fuq)
 # You can use whatever you want as an alias, like for Mondays:
 eval $(thefuck --alias FUCK)
-alias nvim='lvim'
+# alias nvim='lvim'
 
 # make sure to install zoxide with 'sudo pacman -S zoxide'
 alias cd='z'
