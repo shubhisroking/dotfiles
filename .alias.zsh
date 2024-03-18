345a89ea13cb0db1543dee8c84cbb4ea5e29245c
Shubh <shubhisroking@proton.me>
2024-03-18 15:40:22 +0530

    add trash cli alias
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- .alias.zsh
+++ .alias.zsh
@@ -2,11 +2,9 @@ alias ll='ls -lh'
 alias l='ls -Alh'
 alias vi='nvim'
 alias vim='nvim'
-eval $(thefuck --alias)
-eval $(thefuck --alias fuq)
-# You can use whatever you want as an alias, like for Mondays:
-eval $(thefuck --alias FUCK)
-# alias nvim='lvim'
 
-# make sure to install zoxide with 'sudo pacman -S zoxide'
+# NOTE make sure to install zoxide with 'sudo pacman -S zoxide'
 alias cd='z'
+
+# NOTE make sure to install trash-cli with `sudo pacman -S  trash-cli`
+alias rm='trash'
