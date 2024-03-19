bea24c62c135c56ced52fbfaea1606916c22c724
Shubh <shubhisroking@proton.me>
2024-03-19 19:37:14 +0530

    [autostart.sh] use xss-lock instead of xautolock
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- .xmonad/autostart.sh
+++ .xmonad/autostart.sh
@@ -1,17 +1,8 @@
-# Merge X resources
 xrdb -merge ~/.Xresources &
-
-# Start the compositor, adjust screen color temperature, and enable screen locking
 redshift -P -O 4500 &
-xautolock -time 10 -locker slock &
-
-# Start the notification daemon, clipboard manager, status bar, and set wallpaper
+xss-lock slock &
 dunst &
 greenclip daemon &
 nitrogen --restore &
-
-# Start the Polkit-GNOME authentication agent
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-
-# Set the keyboard repeat rate
 xset r rate 300 50
